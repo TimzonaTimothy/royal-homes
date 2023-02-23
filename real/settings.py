1aa8ef34127a65d1313e34127ee9752e40b39e06
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2023-02-23 18:35:35 +0100

    c

--- real/settings.py
+++ real/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h$aqu%q6z)*()*143ts=kq+i^bl-34-%7gj8fpb78*$*v6izah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*",]
 
